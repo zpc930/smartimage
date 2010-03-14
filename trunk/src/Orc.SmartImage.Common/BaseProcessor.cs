f7ad4f28416933fda47680abe35013f614767f34
xiaotie <xiaotie@b70c862e-eb66-11de-b92f-5d3588262692>
2010-03-14 16:17:09 +0000

    高性能C#图像处理

--- trunk/src/Orc.SmartImage.Common/BaseProcessor.cs
+++ trunk/src/Orc.SmartImage.Common/BaseProcessor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using System.Drawing;
-using Emgu.CV;
 
 namespace Orc.SmartImage
 {
---
+++ trunk/src/Orc.SmartImage.Common/IColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Orc.SmartImage
+{
+    public interface IColor
+    {
+        unsafe void Copy(Byte* from, Argb32* to);
+        unsafe void Copy(Argb32* from, Byte* to);
+    }
+}
